cfd2d011f6ff5d6a99e656994e1f4a59875e59e6
Manthan Sharma <72844165+man-than-shar-ma@users.noreply.github.com>
2021-11-02 21:55:16 +0530

    c1

--- c1/Walker.js
+++ c1/Walker.js
@@ -1,29 +1,30 @@
 class Walker{
-    constructor(width,height){
+    constructor(width,height,rad){
         this.x = width/2;
         this.y = height/2;
+        this.rad = rad;
     }
 
-    display(){
-        stroke(255);
-        strokeWeight(5);
-        point(this.x,this.y);
+    display(can,col){
+        can.stroke(col);
+        can.strokeWeight(this.rad);
+        can.point(this.x,this.y);
     }
 
     step(){
         let ch = floor(random(4));
         switch(ch){
             case 0:
-                this.x += 5;
+                this.x += this.rad;
                 break;
             case 1:
-                this.x -= 5;
+                this.x -= this.rad;
                 break;
             case 2:
-                this.y += 5;
+                this.y += this.rad;
                 break;
             case 3:
-                this.y -= 5;
+                this.y -= this.rad;
                 break;    
         }
     }
--- c1/sketch.js
+++ c1/sketch.js
@@ -1,14 +1,20 @@
 let walker1;
+let og;
+let rad = 5;
 
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
-  walker1 = new Walker(width,height);
+  og = createGraphics(windowWidth,windowHeight);
+  walker1 = new Walker(width,height,rad,og);
   background(0);
+  og.clear();
 }
 
 function draw() {
   // put drawing code here
   walker1.step();
-  walker1.display();
+  walker1.display(og, 255);
+  image(og,0,0);
+  walker1.display(this,color(255,0,0));
 }
